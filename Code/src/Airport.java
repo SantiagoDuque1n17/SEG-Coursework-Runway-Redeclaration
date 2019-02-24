81bf2d22c0399aee4e9974de2300b5a9b066b02a
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-02-24 23:43:24 +0000

    Create Airport.java

---
+++ Code/src/Airport.java
@@ -0,0 +1,11 @@
+public class Airport {
+    private String name;
+    private String id;
+
+    public Airport(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    //TODO: Possibly a list of Runways as variable as well?
+}
