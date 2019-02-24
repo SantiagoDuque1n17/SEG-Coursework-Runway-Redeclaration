67df122404e3a39dd9626c8b1cd6c846af1c3e52
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-02-24 23:39:05 +0000

    Short Obstacle class

---
+++ Code/src/Obstacle.java
@@ -0,0 +1,9 @@
+public class Obstacle {
+    private int height;
+    private String name;
+
+    public Obstacle(int height, String name) {
+        this.height = height;
+        this.name = name;
+    }
+}
