fe8cbe881b00bb14fd7b9b8d0ede7feda067983c
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-02-24 23:37:00 +0000

    Added variables with short description

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
+         private String RunwayDesignator; // Code for runways, e.g.: 09L
+        private int LDA; //Landing distance available
+        private int RESA; // Runway	End	Safety	Area
+        private int TORA; // Take-Off Run Available
+        private int TODA; // Take-Off Distance Available
+        private int ASDA; // Landing distance available
+        private int DisplacedThreshold; //
+        private int TOCS; // Take-Off Climb Surface
+        private int ALS; // Approach-Landing Surface
     }
 }
