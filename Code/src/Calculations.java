b1fddf1090acd9d359fabf78118815dc9e5ab7d1
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-02-25 12:42:50 +0000

    Calculations class with stub function

---
+++ Code/src/Calculations.java
@@ -0,0 +1,13 @@
+public class Calculations {
+
+    private void landingOverObstacle(Runway r, Obstacle o) {
+        
+        //TODO: Proper calculations
+
+        r.setASDA(1);
+        r.setLDA(1);
+        r.setTORA(1);
+        r.setTODA(1);
+    }
+
+}
