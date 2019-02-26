7dc842a2755e8b3944a4aea1a5fbd9bf0b4c1171
DOMInick0123 <dominick.vranay@gmail.com>
2019-02-26 10:49:12 +0000

    Started with take-off away from the obstacle calculations

--- Code/src/Calculations.java
+++ Code/src/Calculations.java
@@ -13,4 +13,18 @@ public class Calculations {
         r.setLDA(newLDA);
     }
 
+    public void takeoffAwayObstacle(Runway r, Obstacle o, int distanceToThreshold) {
+        int currentTORA = r.getTORA;
+        int limitation = 300 + distanceToThreshold
+        int newTORA = currentTORA - limitation;
+        int
+
+        if (newTORA <=0) {
+            //TODO: exceptions
+        }
+        r.setTORA(newTORA);
+        r.setTODA(r.getTODA-limitation);
+        r.setASDA(r.getASDA-limitation);
+        //TODO: add better calculations
+    }
 }
