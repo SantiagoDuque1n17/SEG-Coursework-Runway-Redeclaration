db1fcf0989bda806350f417489290826c625cf51
DOMInick0123 <dominick.vranay@gmail.com>
2019-02-27 14:33:11 +0000

    Removed length and width because we don't need them

--- Code/src/Obstacle.java
+++ Code/src/Obstacle.java
@@ -1,26 +1,14 @@
 public class Obstacle {
     private int height;
-    private int width;
-    private int length;
     private String name;
 
-    public Obstacle(int height, int width, int length, String name) {
+    public Obstacle(int height, String name) {
         this.height = height;
-        this.width = width;
-        this.length = length;
         this.name = name;
     }
 
     public int getHeight() {
         return height;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public int getWidth() {
-        return width;
     }
 
     public String getName() {
