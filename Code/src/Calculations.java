38904c4012941d197c33b09fc916acdee2f1bc49
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-02-27 23:30:37 +0000

    Added function to check if redeclaration needed
    Also implemented the distance to threshold as an attribute of an obstacle rather than as a parameter

--- Code/src/Calculations.java
+++ Code/src/Calculations.java
@@ -1,37 +1,53 @@
-import RunwayRedeclaration.Exceptions.NegativeParameterException;
+import RunwayRedeclaration.Exceptions;
 
 public class Calculations {
     private static final int blastProtection = 300;
     private static final int RESA = 240;
     private static final int stripEnd = 60;
+    private static final int sideSpace = 75;
 
-    public void landingOverObstacle(Runway r, Obstacle o, int distanceToThreshold) throws NegativeParameterException {
+    private boolean isRedeclarationNeeded(Obstacle o) {
+        if (o.getDistToThreshold() > stripEnd || o.getDistToCentreline() > sideSpace) {
+            return false;
+        }
+        return true;
+    }
 
-        //TODO: Refine calculations
+    public void landingOverObstacle(Runway r, Obstacle o) throws NegativeParameterException, DontNeedRedeclarationException {
+
+        if (isRedeclarationNeeded(o)) {
+            throw new DontNeedRedeclarationException();
+        }
 
         int originalLDA = r.getOriginalLDA();
         int slopeCalc = RESA;
         if ((50*o.getHeight())>RESA)
             slopeCalc = 50*o.getHeight();
-        int newLDA = originalLDA - distanceToThreshold - slopeCalc - stripEnd;
+
+        int newLDA = originalLDA - o.getDistToThreshold() - slopeCalc - stripEnd;
 
         if (newLDA <= 0) {
-            throw new NegativeParameterException("Obstacle dimensions too great, can't redeclare, can't use runway.");
+            throw new NegativeParameterException();
         }
         r.setLDA(newLDA);
     }
 
-    public void takeOffAwayObstacle(Runway r, int distanceToThreshold) {
+    public void takeOffAwayObstacle(Runway r, Obstacle o) throws NegativeParameterException, DontNeedRedeclarationException {
+
+        if (isRedeclarationNeeded(o)) {
+            throw new DontNeedRedeclarationException();
+        }
+
         int originalTORA = r.getOriginalTORA();
         int displacedThreshold = r.getDisplacedThreshold();
         int clearway = r.getClearway();
         int stopway = r.getStopway();
-        int newTORA = originalTORA - blastProtection - distanceToThreshold - displacedThreshold;
+        int newTORA = originalTORA - blastProtection - o.getDistToThreshold() - displacedThreshold;
         int newTODA = newTORA + clearway;
         int newASDA = newTORA + stopway;
 
         if (newTORA <=0) {
-            //TODO: exceptions
+            throw new NegativeParameterException();
         }
         r.setTORA(newTORA);
         r.setTODA(newTODA);
@@ -39,14 +55,23 @@ public class Calculations {
         //TODO: add better calculations
     }
 
-    public void landingTowardsObstacle(Runway r, int distanceToThreshold) {
-        int newLDA = distanceToThreshold - RESA - stripEnd;
+    public void landingTowardsObstacle(Runway r, Obstacle o) throws DontNeedRedeclarationException {
+
+        if (isRedeclarationNeeded(o)) {
+            throw new DontNeedRedeclarationException();
+        }
+
+        int newLDA = o.getDistToThreshold() - RESA - stripEnd;
         if (r.getOriginalLDA()>newLDA)
             r.setLDA(newLDA);
     }
 
-    public void takeOffTowardsObstacle(Runway r, Obstacle o, int distanceToThreshold)
-    {
+    public void takeOffTowardsObstacle(Runway r, Obstacle o) throws DontNeedRedeclarationException {
+
+        if (isRedeclarationNeeded(o)) {
+            throw new DontNeedRedeclarationException();
+        }
+
         int currentTORA = r.getTORA();
         int currentTODA = r.getTODA();
         int currentASDA = r.getASDA();
@@ -60,7 +85,7 @@ public class Calculations {
         if ((50*o.getHeight())>RESA)
             tempThreshold = 50*o.getHeight();
 
-        newTORA = distanceToThreshold + displacedThreshold - tempThreshold - 60;
+        newTORA = o.getDistToThreshold() + displacedThreshold - tempThreshold - 60;
         newTODA = newTORA;
         newASDA = newTORA;
 
