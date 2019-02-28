7a2bd86eb0620a7ba558f96f1edfec0dc90d7a21
DOMInick0123 <dominick.vranay@gmail.com>
2019-02-28 13:36:26 +0000

    Added list of physical runways to the airport

--- Code/src/Airport.java
+++ Code/src/Airport.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Airport {
     private String name;
     private String id;
+    private List<PhysicalRunway> runways;
 
     public Airport(String name, String id) {
         this.name = name;
         this.id = id;
+        runways = new ArrayList<>();
     }
 
+    public void addRunway(PhysicalRunway runway) {
+        runways.add(runway);
+    }
     //TODO: Possibly a list of Runways as variable as well?
 }
