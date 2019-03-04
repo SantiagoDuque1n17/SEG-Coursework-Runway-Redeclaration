ad40c59f8ef21bf14463dfd78c2f95a69a9ea8a2
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-03-04 10:31:16 +0000

    Fixed runway names

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,14 +1,13 @@
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Main {
     public static void main(String[] args) {
 
         List <PhysicalRunway> runwayList = new ArrayList<PhysicalRunway>();
         runwayList.add( new PhysicalRunway(
                             new Runway("27R", 3884, 3884, 3962, 3884, 0),
-                            new Runway("90L", 3684, 3684, 3952, 3884, 0)
+                            new Runway("09L", 3595, 3902, 3902, 3902, 306)
                       )
         );
         runwayList.add( new PhysicalRunway(
@@ -18,13 +17,13 @@ public class Main {
         );
         runwayList.add( new PhysicalRunway(
                         new Runway("27L", 3844, 3844, 3862, 3284, 0),
-                        new Runway("90R", 3684, 3684, 3952, 3884, 0)
+                        new Runway("09R", 3684, 3684, 3952, 3884, 0)
                 )
         );
 
 
         List<Obstacle> obstacleList = new ArrayList<Obstacle>();
-        obstacleList.add(new Obstacle(5, "Bus", 0,0,0));
+        obstacleList.add(new Obstacle(12, "Bus", 3646,-50,0));
         obstacleList.add(new Obstacle(20, "Crashed plane", 0,0,0));
         obstacleList.add(new Obstacle(10, "Debris", 0,0,0));
 
@@ -32,3 +31,4 @@ public class Main {
         i.run();
     }
 }
+
