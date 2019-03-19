127b8aaf0a633753df2b2e725a86047239040612
sdp1n17 <34222605+SpanishDuke@users.noreply.github.com>
2019-03-19 13:17:59 +0000

    Update AddObstacleController.java

--- Code/src/Controller/AddObstacleController.java
+++ Code/src/Controller/AddObstacleController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class AddObstacleController
 {
     Obstacle obstacle;
@@ -25,6 +28,11 @@ public class AddObstacleController
         {
             obstacle = new Obstacle(obsName.getText().trim(), Integer.parseInt(obsHeight.getText().trim()), 0, 0, 0);
             controller.addObs(obstacle);
+
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("[HH:mm:ss]");
+            controller.setSystemLogText(sdf.format(cal.getTime()) + " Obstacle " + "\"" + obstacle.getName() + "\" added to list.");
+
             ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
         }
         else
